ca5ac4ddd0445dcb5713270b52b3c395f175db0b
Somaycon <contato.somaycon@gmail.com>
2026-09-17 20:36:17 -0300

    feat: Load .env, configure PostgresSQL and Redis

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	appPort := os.Getenv("APP_PORT")
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPass := os.Getenv("DATABASE_PASSWORD")
+	dbName := os.Getenv("DATABASE_NAME")
+	redisHost := os.Getenv("REDIS_HOST")
+	redisPort := os.Getenv("REDIS_PORT")
+	jwtSecret := os.Getenv("JWT_SECRET")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPass, dbName)
+
+	fmt.Println("App Port:", appPort)
+	fmt.Println("Redis Host:", redisHost)
+	fmt.Println("Redis Port:", redisPort)
+	fmt.Println("JWT Secret:", jwtSecret)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Error connecting to database:", err)
+	}
+	db.AutoMigrate()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisHost + ":" + redisPort,
+		Password: "",
+		DB:       0,
+	})
+	fmt.Println("Redis Client:", rdb)
 
 }
